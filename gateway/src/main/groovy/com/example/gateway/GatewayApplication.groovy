eb08d8619e0e96b632d2ebb67fe8d91791db46eb
Zhenzhen Wang <zwang211@dons.usfca.edu>
2022-04-05 15:39:51 -0700

    change to new anotation

--- gateway/src/main/groovy/com/example/gateway/GatewayApplication.groovy
+++ gateway/src/main/groovy/com/example/gateway/GatewayApplication.groovy
@@ -2,10 +2,9 @@ package com.example.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+
 
 @SpringBootApplication
-@EnableCircuitBreaker
 class GatewayApplication {
 
 	static void main(String[] args) {
--- service1/src/main/groovy/com/example/Application.groovy
+++ service1/src/main/groovy/com/example/Application.groovy
@@ -1,11 +1,12 @@
 package com.example
 
+import io.micronaut.retry.annotation.CircuitBreaker
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 
 @CompileStatic
-@EnableCircuitBreaker
+@CircuitBreaker
 class Application {
     static void main(String[] args) {
         Micronaut.run(Application, args)
