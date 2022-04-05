59037e812fb8775e8cf504e7c47134d522df04b9
Zhenzhen Wang <zwang211@dons.usfca.edu>
2022-04-05 15:25:05 -0700

    adding Hystrix dependencies to microservice

--- service1/src/main/groovy/com/example/Application.groovy
+++ service1/src/main/groovy/com/example/Application.groovy
@@ -4,6 +4,7 @@ import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
 
 @CompileStatic
+
 class Application {
     static void main(String[] args) {
         Micronaut.run(Application, args)
