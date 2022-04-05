8bbad1b1b9a87b8b4848b9f2a4a5da365f2ae541
Zhenzhen Wang <zwang211@dons.usfca.edu>
2022-04-05 15:31:03 -0700

    add @EnableCircuitBreaker to applicaiton class

--- gateway/src/main/groovy/com/example/gateway/GatewayApplication.groovy
+++ gateway/src/main/groovy/com/example/gateway/GatewayApplication.groovy
@@ -2,8 +2,10 @@ package com.example.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 
 @SpringBootApplication
+@EnableCircuitBreaker
 class GatewayApplication {
 
 	static void main(String[] args) {
--- service1/src/main/groovy/com/example/Application.groovy
+++ service1/src/main/groovy/com/example/Application.groovy
@@ -2,9 +2,10 @@ package com.example
 
 import io.micronaut.runtime.Micronaut
 import groovy.transform.CompileStatic
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 
 @CompileStatic
-
+@EnableCircuitBreaker
 class Application {
     static void main(String[] args) {
         Micronaut.run(Application, args)
